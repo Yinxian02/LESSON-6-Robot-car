fbe5c84672abe327ce24e1b71fd89292b75acad2
Yinxian02 <103280182+Yinxian02@users.noreply.github.com>
2024-02-25 12:15:27 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("back")
+    basic.pause(1000)
+    radio.sendString("stop")
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendString("left")
+    basic.pause(1000)
+    radio.sendString("stop")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showIcon(IconNames.Happy)
+    if (receivedString == "left") {
+        pins.servoWritePin(AnalogPin.P0, 180)
+        pins.servoWritePin(AnalogPin.P1, 180)
+    }
+    if (receivedString == "right") {
+        pins.servoWritePin(AnalogPin.P0, 0)
+        pins.servoWritePin(AnalogPin.P1, 0)
+    }
+    if (receivedString == "back") {
+        pins.servoWritePin(AnalogPin.P0, 0)
+        pins.servoWritePin(AnalogPin.P1, 180)
+    }
+    if (receivedString == "front") {
+        pins.servoWritePin(AnalogPin.P0, 180)
+        pins.servoWritePin(AnalogPin.P1, 0)
+    }
+    if (receivedString == "stop") {
+        pins.servoWritePin(AnalogPin.P0, 90)
+        pins.servoWritePin(AnalogPin.P1, 90)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("front")
+    basic.pause(1000)
+    radio.sendString("stop")
+})
+input.onGesture(Gesture.TiltRight, function () {
+    radio.sendString("right")
+    basic.pause(1000)
+    radio.sendString("stop")
+})
+basic.showIcon(IconNames.Square)
+radio.setGroup(1)
